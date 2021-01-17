79404b6cbb1e9e56b51c76587dfc28dd244a2984
Seanyo89 <seanolawrence_1989@icloud.com>
2021-01-16 20:19:21 -0600

    first commit

---
+++ lib/weather_discover.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "weather_discover/version"
+
+module WeatherDiscover
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/weather_discover/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module WeatherDiscover
+  VERSION = "0.1.0"
+end
